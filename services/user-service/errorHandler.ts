4127c5020737d520e7b1c4cf289c60749a1f220b
aweijun <weijun.ang99@gmail.com>
2023-10-15 17:03:30 +0800

    set up docker compose

--- services/user-service/errorHandler.ts
+++ services/user-service/errorHandler.ts
@@ -1,13 +1,14 @@
 import type { NextFunction, Request, Response } from "express";
-import HttpError from "./error/index";
+
+import type HttpError from "./error/index";
 
 export function errorHandler(
   error: HttpError,
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) {
   // Handle the error here and send an appropriate response
   // Customize the response based on the error
-  res.status(error.statusCode).json({ error: error.message });
+  res.status(500).json({ error: error.message });
 }
