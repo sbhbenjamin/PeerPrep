87642e8dee4e0e25246723bd9ab12bb02eba1b85
aweijun <weijun.ang99@gmail.com>
2023-10-16 17:15:31 +0800

    add test

--- services/user-service/test/unit.test.ts
+++
@@ -1,9 +0,0 @@
-import * as userRepository from "../data-access/user-repository";
-
-afterEach(async () => {
-  await userRepository.cleanupData();
-});
-
-test("add User", () => {
-  // userRepository.addUser({ name: "wei jun", email: "weijun.com@gmail.com" });
-});
