506d19987d5f33d45aa8b9b533b820a1481899f4
aweijun <weijun.ang99@gmail.com>
2023-10-22 19:01:02 +0800

    Refactor

--- services/user-service/test/e2e.test.ts
+++ services/user-service/test/e2e.test.ts
@@ -15,32 +15,93 @@ beforeEach(async () => {
   await resetDb();
 });
 
+const invalidEmail = "che@ky";
+
+const invalidUserInputOnlyName = { name: "wei jun" };
+const invalidUserInputOnlyEmail = { email: "weijun@gmail.com" };
+const createUserInputFull = {
+  name: "james chua",
+  email: "james@gmail.com",
+  bio: "Chicken Nuggs",
+  url: "https://chat.openai.com/",
+};
 const createUserInput = { name: "wei jun", email: "weijun@gmail.com" };
 const createUserInput1 = { name: "wei jun", email: "weijun1@gmail.com" };
 const createUserInput2 = { name: "wei ming", email: "weiming@gmail.com" };
-const createUserInput3 = { name: "xiao ming", email: "xiaoming@gmail.com" };
 
 describe("POST /user", () => {
-  test("POST /user", async () => {
+  test("when valid name, email, bio and url are provided, return status 200 OK", async () => {
     // act
-    const res = await mockApp.post("/user").send(createUserInput);
+    const res = await mockApp.post("/user").send(createUserInputFull);
     const user = await prisma.user.findUnique({
-      where: { ...createUserInput },
+      where: { ...createUserInputFull },
     });
-
     // assert
     expect(res.status).toBe(200);
     expect(user).not.toBe(null);
   });
 
-  test("POST /user [Duplicate]", async () => {
+  test("when valid name and email are provided, return status 200 OK", async () => {
+    // act
+    const res = await mockApp.post("/user").send(createUserInput);
+    const user = await prisma.user.findUnique({
+      where: { ...createUserInput },
+    });
+    // assert
+    expect(res.status).toBe(200);
+    expect(user).not.toBe(null);
+  });
+
+  test("when only email is provided, return status 400 Bad Request", async () => {
+    // act
+    const res = await mockApp.post("/user").send(invalidUserInputOnlyEmail);
+    const user = await prisma.user.findUnique({
+      where: { ...invalidUserInputOnlyEmail },
+    });
+    // assert
+    expect(res.status).toBe(400);
+    expect(user).toBe(null);
+  });
+
+  test("when only name is provided, return status 400 Bad Request", async () => {
+    // act
+    const res = await mockApp.post("/user").send(invalidUserInputOnlyName);
+    const user = await prisma.user.findMany({
+      where: { ...invalidUserInputOnlyName },
+    });
+    // assert
+    expect(res.status).toBe(400);
+    expect(user).toEqual([]);
+  });
+
+  test("when invalid email is provided, return status 400 Bad Request", async () => {
+    // act
+    const res = await mockApp.post("/user").send(invalidUserInputOnlyName);
+    const user = await prisma.user.findMany({
+      where: { ...createUserInput, email: invalidEmail },
+    });
+    // assert
+    expect(res.status).toBe(400);
+    expect(user).toEqual([]);
+  });
+
+  test("when invalid url is provided, return status 400 Bad Request", async () => {
+    // act
+    const res = await mockApp.post("/user").send(invalidUserInputOnlyName);
+    const user = await prisma.user.findMany({
+      where: { ...createUserInputFull, url: "help" },
+    });
+    // assert
+    expect(res.status).toBe(400);
+    expect(user).toEqual([]);
+  });
+
+  test("when there is already a user with the same email, return status 409 Conflict", async () => {
     // arrange
     await mockApp.post("/user").send(createUserInput);
-
     // act
     const res = await mockApp.post("/user").send(createUserInput);
     const users = await prisma.user.findMany({ where: { ...createUserInput } });
-
     // assert
     expect(res.status).toBe(409);
     expect(users.length).toBe(1);
@@ -48,22 +109,19 @@ describe("POST /user", () => {
 });
 
 describe("GET /user", () => {
-  test("GET /user", async () => {
+  test("when calling the api, return status 200 OK", async () => {
     // arrange
     await prisma.user.create({ data: createUserInput });
     const user = await prisma.user.findMany();
-
     // action
     const res = await mockApp.get("/user");
-
     // assert
     expect(res.status).toBe(200);
     expect(res.body).toEqual(user);
   });
 
-  test("GET /user [Filtering]", async () => {
+  test("when adding email filter, return status 200 OK", async () => {
     const findEmail = "weijun@gmail.com";
-
     // arrange
     await prisma.user.createMany({
       data: [createUserInput, createUserInput1, createUserInput2],
@@ -71,10 +129,8 @@ describe("GET /user", () => {
     const users = await prisma.user.findMany({
       where: { email: findEmail },
     });
-
     // action
     const res = await mockApp.get(`/user?email=${findEmail}`);
-
     // assert
     expect(res.status).toBe(200);
     expect(res.body).toEqual(users);
@@ -82,13 +138,12 @@ describe("GET /user", () => {
 });
 
 describe("PUT /user", () => {
-  test("PUT /user", async () => {
+  test("When editing users with the correct params, return status 200 OK", async () => {
     // arrange
     await prisma.user.create({ data: createUserInput });
     const beforeModified = await prisma.user.findUnique({
       where: createUserInput,
     });
-
     // action
     const res = await mockApp
       .put(`/user/${beforeModified!.id}`)
@@ -98,7 +153,6 @@ describe("PUT /user", () => {
         id: beforeModified!.id,
       },
     });
-
     // assert
     expect(res.status).toBe(200);
     expect(afterModified?.name).toBe("new name");
