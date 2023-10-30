ef6ee2d1b73e45f990552a64cb9dbf95e2b720b6
aweijun <weijun.ang99@gmail.com>
2023-10-30 21:58:50 +0800

    Clean

--- services/user-service/src/commons/auth/authenticator.ts
+++ services/user-service/src/commons/auth/authenticator.ts
@@ -8,7 +8,7 @@ export async function authenticationCheck(req: Request) {
   // If you don't have NEXTAUTH_SECRET set, you will have to pass your secret as `secret` to `getToken`
   const token = await getToken({
     req,
-    secret: "RNhSa7yMYad0tp64xNhI/CWnPB/9neUnH7+fPFnba/w=",
+    secret: process.env.NEXTAUTH_SECRET,
   });
   if (token == null && process.env.NODE_ENV !== "test") {
     throw new HttpError("This is a protected route", 404);
