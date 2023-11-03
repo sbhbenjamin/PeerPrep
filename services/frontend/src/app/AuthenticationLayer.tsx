c90c88fa616bf5ec89685237b043844d80baa2f3
aweijun <weijun.ang99@gmail.com>
2023-11-03 13:29:41 +0800

    Clean code

--- services/frontend/src/app/AuthenticationLayer.tsx
+++ services/frontend/src/app/AuthenticationLayer.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { toast } from "sonner";
 
 import { sessionSignIn } from "@/features/auth";
 
@@ -21,24 +22,29 @@ const AuthenticationLayer = ({ children }: Props) => {
     return response.json();
   };
 
-  useEffect(() => {
-    const getUserInfo = async () => {
-      if (session) {
+  const dispatchSignInOrRedirect = async () => {
+    try {
+      if (session && session.currentUser) {
+        const sessionToken = await fetchSessionToken();
         dispatch(
           sessionSignIn({
             currentUser: session.currentUser,
             image: session.user?.image || null,
-            sessionToken: await fetchSessionToken(),
+            sessionToken,
             isLoggedIn: true,
           }),
         );
-
-        if (session.currentUser == null) {
-          router.push("/onboarding");
-        }
       }
-    };
-    getUserInfo();
+      if (session && session.currentUser == null) {
+        router.push("/onboarding");
+      }
+    } catch (e) {
+      toast.error("Unable to fetch token");
+    }
+  };
+
+  useEffect(() => {
+    dispatchSignInOrRedirect();
   }, [session, dispatch, router]);
 
   return children;
