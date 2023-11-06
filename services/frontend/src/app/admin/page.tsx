3ed5bf17cc3051efcd5e742fc2a7e4a5777344d8
aweijun <weijun.ang99@gmail.com>
2023-11-06 20:49:46 +0800

    Date

--- services/frontend/src/app/admin/page.tsx
+++ services/frontend/src/app/admin/page.tsx
@@ -1,20 +1,37 @@
 import React from "react";
 
-import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const page = () => {
   return (
     <div>
-      <Table />
-      <TableHeader>
-        <TableRow>
-          <TableHead>Display Name</TableHead>
-          <TableHead>Role</TableHead>
-          <TableHead>Email</TableHead>
-          <TableHead>Url</TableHead>
-          <TableHead>Bio</TableHead>
-        </TableRow>
-      </TableHeader>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Display Name</TableHead>
+            <TableHead>Role</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead>Url</TableHead>
+            <TableHead>Bio</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>Wei Jun</TableCell>
+            <TableCell>Admin</TableCell>
+            <TableCell>ang.weijun1999@gmail.com</TableCell>
+            <TableCell>null</TableCell>
+            <TableCell>Wei Jun</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
   );
 };
