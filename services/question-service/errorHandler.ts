b976e1fc89ac4f4b8fc823b23084ae855a4a5781
Jonathan Tan <xinjie.tan@thecarousell.com>
2023-11-09 15:51:33 +0800

    fix error code

--- services/question-service/errorHandler.ts
+++ services/question-service/errorHandler.ts
@@ -1,7 +1,9 @@
 import type { NextFunction, Request, Response } from "express";
 
+import type HttpError from "./src/commons/error";
+
 export function errorHandler(
-  error: Error,
+  error: HttpError,
   req: Request,
   res: Response,
   next: NextFunction,
@@ -9,5 +11,5 @@ export function errorHandler(
   // Handle the error here and send an appropriate response
   console.error(error); // Log the error for debugging purposes
   // Customize the response based on the error
-  res.status(500).json({ error: error.message });
+  res.status(error.statusCode).json({ error: error.message });
 }
