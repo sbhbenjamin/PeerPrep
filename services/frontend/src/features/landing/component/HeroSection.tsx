8a102ee2e63105375ed4576c6e59801f18d3e382
Ang Wei Jun <weijun.ang1999@gmail.com>
2023-11-13 01:13:48 +0800

    Test

--- services/frontend/src/features/landing/component/HeroSection.tsx
+++ services/frontend/src/features/landing/component/HeroSection.tsx
@@ -1,34 +1,25 @@
 import React from "react";
 
+import { Button } from "@/components/ui/button";
+
 const HeroSection = () => {
   return (
-    <div className="flex min-h-screen items-center bg-white p-8 md:p-16">
-      <div className=" mx-auto mt-10 flex h-full flex-col-reverse items-center gap-6 space-y-0 px-6 md:flex-row md:gap-7 md:space-y-6">
+    <div className="flex min-h-screen items-center ">
+      <div className=" mx-auto mt-10 flex h-full flex-col-reverse items-center gap-6 space-y-0 px-6 md:flex-row md:gap-10 md:space-y-6">
         <div className="flex flex-col space-y-6 md:w-1/2">
-          <h1 className="max-w-md text-center text-3xl font-bold text-black md:text-left md:text-4xl">
+          <h1 className="max-w-md text-center text-3xl font-bold  md:text-left md:text-4xl">
             The Ultimate Guide to Acing Tech Interviews
           </h1>
-          <p className="text-md max-w-sm text-center text-gray-600 md:text-left md:text-2xl">
+          <p className="max-w-sm text-center  md:text-left md:text-2xl">
             Prepare with confidence, perform with excellence, and land your
             ideal tech role.
           </p>
           <div className="flex justify-center gap-4 md:justify-start">
-            <a
-              href="/"
-              className=" md:text-md  rounded-full p-3 text-sm text-black"
-            >
-              Start Preparing
-            </a>
-            <a
-              href="/"
-              className=" md:text-md rounded-full border border-black p-3 text-sm text-black"
-            >
-              Learn More
-            </a>
+            <Button>Start Now</Button>
           </div>
         </div>
         <div className="md:w-1/2">
-          <img src="peer-prep.jpg" alt="logo" />
+          <img src="/peerpreplogo.svg" alt="logo" />
         </div>
       </div>
     </div>
--- services/frontend/src/features/landing/component/InformationSection.tsx
+++ services/frontend/src/features/landing/component/InformationSection.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 export const InformationSection = () => {
   return (
-    <div className="max-h-screen bg-white py-24 sm:py-32">
+    <div className="max-h-screen py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
-          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          <p className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
             Elevate Your Interview Prep Experience
           </p>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
+          <p className="mt-6 text-lg leading-8 ">
             Elevate Your Interview Prep Experience with our comprehensive
             question database, intelligent matching system, realistic interview
             simulations, and instant performance analysis.
@@ -17,40 +17,23 @@ export const InformationSection = () => {
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
           <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
             <div className="relative pl-16">
-              <dt className="text-base font-semibold leading-7 text-gray-900">
-                <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                  <svg
-                    className="h-6 w-6 text-white"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  />
-                </div>
+              <dt
+                className="text-base font-semibold leading-7
+              "
+              >
                 Unlock Our Extensive Question Database
               </dt>
-              <p className="mt-2 text-base leading-7 text-gray-600">
+              <p className="mt-2 text-base leading-7 ">
                 Explore our comprehensive question database, tailored to cover a
                 wide range of technical interview categories and varying levels
                 of difficulty.
               </p>
             </div>
             <div className="relative pl-16">
-              <dt className="text-base font-semibold leading-7 text-gray-900">
-                <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                  <svg
-                    className="h-6 w-6 text-white"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  />
-                </div>
+              <dt className="text-base font-semibold leading-7 ">
                 Smart Matching by Category and Difficulty
               </dt>
-              <p className="mt-2 text-base leading-7 text-gray-600">
+              <p className="mt-2 text-base leading-7 ">
                 Our intelligent matching system pairs you with interview
                 questions based on your preferences. Select from various
                 question categories and difficulty levels, and we will curate
@@ -58,20 +41,10 @@ export const InformationSection = () => {
               </p>
             </div>
             <div className="relative pl-16">
-              <dt className="text-base font-semibold leading-7 text-gray-900">
-                <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                  <svg
-                    className="h-6 w-6 text-white"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  />
-                </div>
+              <dt className="text-base font-semibold leading-7 ">
                 Realistic Interview Experience
               </dt>
-              <p className="mt-2 text-base leading-7 text-gray-600">
+              <p className="mt-2 text-base leading-7 ">
                 Experience the authenticity of real technical interviews with
                 our chat-based communication feature. Engage in conversations
                 with interviewers through our platform, simulating real
@@ -79,20 +52,10 @@ export const InformationSection = () => {
               </p>
             </div>
             <div className="relative pl-16">
-              <dt className="text-base font-semibold leading-7 text-gray-900">
-                <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                  <svg
-                    className="h-6 w-6 text-white"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  />
-                </div>
+              <dt className="text-base font-semibold leading-7 ">
                 Instant Feedback and Analysis
               </dt>
-              <p className="mt-2 text-base leading-7 text-gray-600">
+              <p className="mt-2 text-base leading-7 ">
                 Receive immediate feedback and in-depth performance analysis
                 after each interview session. Understand your strengths and
                 areas for improvement. Focus your preparation where it matters
