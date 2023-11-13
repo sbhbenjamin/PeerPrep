b15190e08e93c98e35bf56b43ee5813c38ddcaf3
Ang Wei Jun <weijun.ang1999@gmail.com>
2023-11-13 23:28:01 +0800

    Test

--- services/frontend/src/app/unauthorized/page.tsx
+++ services/frontend/src/app/unauthorized/page.tsx
@@ -2,14 +2,21 @@
 
 import { useSearchParams } from "next/navigation";
 
+import { Button } from "@/components/ui/button";
+
 const page = () => {
   const message = useSearchParams().get("message");
 
   return (
-    <div className=" h-full w-full bg-slate-100">
-      <h1 className="text-4xl">Unauthorized Access</h1>
+    <div className="flex min-h-[calc(100vh-200px)] flex-col items-center justify-center gap-8">
+      <svg />
+      <h1 className="text-4xl font-semibold">Unauthorized Access</h1>
       <p>You do not have permission to access this page.</p>
       <p>{message}</p>
+      <div>
+        <Button>Back</Button>
+        <Button>Return Home</Button>
+      </div>
     </div>
   );
 };
