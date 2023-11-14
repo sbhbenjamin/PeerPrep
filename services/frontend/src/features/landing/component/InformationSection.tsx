5b2c289afea4ef19e88732af56ccec5d7d5e6333
aweijun <weijun.ang99@gmail.com>
2023-11-15 03:37:26 +0800

    Add landing and hero page

--- services/frontend/src/features/landing/component/InformationSection.tsx
+++ services/frontend/src/features/landing/component/InformationSection.tsx
@@ -2,69 +2,173 @@ import React from "react";
 
 export const InformationSection = () => {
   return (
-    <div className="max-h-screen py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:text-center">
-          <p className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
+    <section className=" bg-white dark:bg-gray-900">
+      <div className="mx-auto max-w-screen-xl gap-10 px-4 py-8 sm:py-16 lg:px-6">
+        <div className="mb-8 max-w-screen-md lg:mb-16">
+          <h2 className="mb-4 text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">
             Elevate Your Interview Prep Experience
-          </p>
-          <p className="mt-6 text-lg leading-8 ">
+          </h2>
+          <p className="text-gray-500 dark:text-gray-400 sm:text-xl">
             Elevate Your Interview Prep Experience with our comprehensive
             question database, intelligent matching system, realistic interview
             simulations, and instant performance analysis.
           </p>
         </div>
-        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-            <div className="relative pl-16">
-              <dt
-                className="text-base font-semibold leading-7
-              "
+        <div className="space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0 lg:grid-cols-3">
+          <div>
+            <div className="bg-primary-100 dark:bg-primary-900 mb-4 flex h-10 w-10 items-center justify-center rounded-full lg:h-12 lg:w-12">
+              <svg
+                className="text-primary-600 dark:text-primary-300 h-5 w-5 lg:h-6 lg:w-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                Unlock Our Extensive Question Database
-              </dt>
-              <p className="mt-2 text-base leading-7 ">
-                Explore our comprehensive question database, tailored to cover a
-                wide range of technical interview categories and varying levels
-                of difficulty.
-              </p>
+                <path
+                  fill-rule="evenodd"
+                  d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
+                  clip-rule="evenodd"
+                />
+              </svg>
             </div>
-            <div className="relative pl-16">
-              <dt className="text-base font-semibold leading-7 ">
-                Smart Matching by Category and Difficulty
-              </dt>
-              <p className="mt-2 text-base leading-7 ">
-                Our intelligent matching system pairs you with interview
-                questions based on your preferences. Select from various
-                question categories and difficulty levels, and we will curate
-                the perfect experience
-              </p>
+            <h3 className="mb-2 text-xl font-bold dark:text-white">
+              Unlock Our Extensive Question Database
+            </h3>
+            <p className="text-gray-500 dark:text-gray-400">
+              Explore our comprehensive question database, tailored to cover a
+              wide range of technical interview categories and varying levels of
+              difficulty.
+            </p>
+          </div>
+          <div>
+            <div className="bg-primary-100 dark:bg-primary-900 mb-4 flex h-10 w-10 items-center justify-center rounded-full lg:h-12 lg:w-12">
+              <svg
+                className="text-primary-600 dark:text-primary-300 h-5 w-5 lg:h-6 lg:w-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z" />
+              </svg>
             </div>
-            <div className="relative pl-16">
-              <dt className="text-base font-semibold leading-7 ">
-                Realistic Interview Experience
-              </dt>
-              <p className="mt-2 text-base leading-7 ">
-                Experience the authenticity of real technical interviews with
-                our chat-based communication feature. Engage in conversations
-                with interviewers through our platform, simulating real
-                interview scenarios.
-              </p>
+            <h3 className="mb-2 text-xl font-bold dark:text-white">
+              Smart Matching by Category and Difficulty
+            </h3>
+            <p className="text-gray-500 dark:text-gray-400">
+              Our intelligent matching system pairs you with interview questions
+              based on your preferences. Select from various question categories
+              and difficulty levels, and we will curate the perfect experience
+            </p>
+          </div>
+          <div>
+            <div className="bg-primary-100 dark:bg-primary-900 mb-4 flex h-10 w-10 items-center justify-center rounded-full lg:h-12 lg:w-12">
+              <svg
+                className="text-primary-600 dark:text-primary-300 h-5 w-5 lg:h-6 lg:w-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                />
+                <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" />
+              </svg>
             </div>
-            <div className="relative pl-16">
-              <dt className="text-base font-semibold leading-7 ">
-                Instant Feedback and Analysis
-              </dt>
-              <p className="mt-2 text-base leading-7 ">
-                Receive immediate feedback and in-depth performance analysis
-                after each interview session. Understand your strengths and
-                areas for improvement. Focus your preparation where it matters
-                most.
-              </p>
+            <h3 className="mb-2 text-xl font-bold dark:text-white">
+              Realistic Interview Experience
+            </h3>
+            <p className="text-gray-500 dark:text-gray-400">
+              Experience the authenticity of real technical interviews with our
+              chat-based communication feature. Engage in conversations with
+              interviewers through our platform, simulating real interview
+              scenarios.
+            </p>
+          </div>
+          <div>
+            <div className="bg-primary-100 dark:bg-primary-900 mb-4 flex h-10 w-10 items-center justify-center rounded-full lg:h-12 lg:w-12">
+              <svg
+                className="h-6 w-6 text-gray-800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4.008 8.714c1-.097 1.96-.45 2.792-1.028a25.112 25.112 0 0 0 4.454-5.72 1.8 1.8 0 0 1 .654-.706 1.742 1.742 0 0 1 1.65-.098 1.82 1.82 0 0 1 .97 1.128c.075.248.097.51.065.767l-1.562 4.629M4.008 8.714H1v9.257c0 .273.106.535.294.728a.99.99 0 0 0 .709.301h1.002a.99.99 0 0 0 .71-.301c.187-.193.293-.455.293-.728V8.714Zm8.02-1.028h4.968c.322 0 .64.08.925.232.286.153.531.374.716.645a2.108 2.108 0 0 1 .242 1.883l-2.36 7.2c-.288.813-.48 1.354-1.884 1.354-2.59 0-5.39-1.06-7.504-1.66"
+                />
+              </svg>
             </div>
-          </dl>
+            <h3 className="mb-2 text-xl font-bold dark:text-white">
+              Instant Feedback and Analysis
+            </h3>
+            <p className="text-gray-500 dark:text-gray-400">
+              Receive immediate feedback and in-depth performance analysis after
+              each interview session. Understand your strengths and areas for
+              improvement. Focus your preparation where it matters most.
+            </p>
+          </div>
+          <div>
+            <div className="bg-primary-100 dark:bg-primary-900 mb-4 flex h-10 w-10 items-center justify-center rounded-full lg:h-12 lg:w-12">
+              <svg
+                className="h-6 w-6 text-gray-800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 16"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M3.656 12.115a3 3 0 0 1 5.682-.015M13 5h3m-3 3h3m-3 3h3M2 1h16a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1Zm6.5 4.5a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"
+                />
+              </svg>
+            </div>
+            <h3 className="mb-2 text-xl font-bold dark:text-white">
+              Personalized Learning Paths
+            </h3>
+            <p className="text-gray-500 dark:text-gray-400">
+              Tailor your study plan with personalized learning paths that adapt
+              to your individual learning style and pace. Maximize your learning
+              efficiency and make the most of your study time.
+            </p>
+          </div>
+          <div>
+            <div className="bg-primary-100 dark:bg-primary-900 mb-4 flex h-10 w-10 items-center justify-center rounded-full lg:h-12 lg:w-12">
+              <svg
+                className="h-6 w-6 text-gray-800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4.333 6.764a3 3 0 1 1 3.141-5.023M2.5 16H1v-2a4 4 0 0 1 4-4m7.379-8.121a3 3 0 1 1 2.976 5M15 10a4 4 0 0 1 4 4v2h-1.761M13 7a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm-4 6h2a4 4 0 0 1 4 4v2H5v-2a4 4 0 0 1 4-4Z"
+                />
+              </svg>
+            </div>
+            <h3 className="mb-2 text-xl font-bold dark:text-white">
+              Community Support and Networking
+            </h3>
+            <p className="text-gray-500 dark:text-gray-400">
+              Join a vibrant community of learners and professionals. Share
+              insights, solve challenges, and network with peers and mentors in
+              your field, fostering both personal growth and professional
+              development.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
